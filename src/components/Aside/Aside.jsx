6bad8f9fdca30fd1cc3bc897a5efd1abcf10cc1b
Ayannagori44 <ayannagori44@gmail.com>
2022-09-04 15:31:54 +0530

    added weekly day tempratures and fixed vids

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,11 +2,56 @@ import React from "react";
 
 import { BsFillSunFill } from "react-icons/bs";
 
+import clear from "../../resources/weathericon/clear.png";
+import wind from "../../resources/weathericon/wind.png";
+import hail from "../../resources/weathericon/hail.png";
+import snow from "../../resources/weathericon/snow.png";
+import fog from "../../resources/weathericon/fog.png";
+import rain from "../../resources/weathericon/rain.png";
+
 import chart from "../../resources/chart.png";
 
 import "./Aside.scss";
 
 const Aside = () => {
+  const weeklyData = [
+    {
+      weather: "Cloudy",
+      date: "November 16",
+      image: clear,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 17",
+      image: rain,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 18",
+      image: fog,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 19",
+      image: hail,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 20",
+      image: clear,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 21",
+      image: rain,
+    },
+    {
+      weather: "Cloudy",
+      date: "November 22",
+      image: wind,
+    },
+  ];
+
   return (
     <div className="Aside">
       <div className="top row">
@@ -20,20 +65,49 @@ const Aside = () => {
         <div className="chart">
           <img src={chart} alt="" />
         </div>
-        <div className="uv flex">
-          <BsFillSunFill
-            style={{
-              color: "var(--primary-color)",
-              fontSize: "3.5rem",
-              marginRight: "1.2rem",
-            }}
-          />
-          <div className="uv-details">
-            <h1 className="badge-heading">
-              20 UVI <span className="badge">Moderate</span>
-            </h1>
-            <p>Moderate risk from UV rays</p>
-          </div>
+      </div>
+
+      <div className="uv flex">
+        <BsFillSunFill
+          style={{
+            color: "var(--primary-color)",
+            fontSize: "3.5rem",
+            marginRight: "1.2rem",
+          }}
+        />
+        <div className="uv-details">
+          <h1 className="badge-heading">
+            20 UVI <span className="badge">Moderate</span>
+          </h1>
+          <p>Moderate risk from UV rays</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <h1>Weather Prediction</h1>
+
+        <div className="predictions">
+          {weeklyData.map((el, i) => {
+            return (
+              <div key={i} className="prediction ">
+                <div className="flex" style={{ height: "100%" }}>
+                  <img
+                    src={el.image}
+                    style={{
+                      fontSize: "3.2rem",
+                      color: "var(--dark-1)",
+                      marginRight: "1.6rem",
+                    }}
+                  />
+                  <div className="predic-details">
+                    <h2>{el.date}</h2>
+                    <h1>{el.weather}</h1>
+                  </div>
+                </div>
+
+                <span className="predic-temp">26° / 19°</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,8 +15,17 @@ const Home = () => {
   const video2Ref = useRef("");
 
   useEffect(() => {
-    video1Ref.current.play();
-    video2Ref.current.play();
+    setTimeout(() => {
+      video1Ref.current.play();
+      video2Ref.current.play();
+    }, 300);
+
+    fetch(
+      // `https://api.openweathermap.org/data/3.0/onecall?lat=51.5072&lon=0.1276&units=metric&exclude=minutely&appid=f1b7ba7a47f863080257931892975f3a`
+      `https://api.openweathermap.org/data/3.0/onecall?lat=51.5072&lon=0.1276&units=metric&exclude=minutely&appid=${process.env.API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }, []);
 
   return (
@@ -24,14 +33,14 @@ const Home = () => {
       <section className="main-section">
         <Navbar />
         {/* Top row */}
-        <div className=" row">
+        <div className="row">
           <div className="col">
             <div className="flex" style={{ marginBottom: "2.2rem" }}>
               <WiCloud
                 style={{
                   backgroundColor: "var(--light-1)",
                   borderRadius: "50%",
-                  fontSize: "4rem",
+                  fontSize: "4.8rem",
                   marginRight: "1.6rem",
 
                   color: "var(--primary-color)",
@@ -72,7 +81,14 @@ const Home = () => {
                 <h2>87%</h2>
               </div>
             </div>
-            <video ref={video1Ref} width="100%" height={"100%"} loop>
+            <video
+              ref={video1Ref}
+              width="100%"
+              height={"100%"}
+              muted
+              loop
+              autoPlay
+            >
               <source src={video1} type="video/mp4" />
             </video>
           </div>
@@ -85,7 +101,7 @@ const Home = () => {
                 style={{
                   backgroundColor: "var(--light-1)",
                   borderRadius: "50%",
-                  fontSize: "4.2rem",
+                  fontSize: "4.8rem",
                   marginRight: "1.5rem",
 
                   color: "var(--primary-color)",
@@ -120,7 +136,14 @@ const Home = () => {
               </div>
             </div>
 
-            <video ref={video2Ref} width="100%" height={"100%"} loop>
+            <video
+              ref={video2Ref}
+              width="100%"
+              height={"100%"}
+              muted
+              loop
+              autoPlay
+            >
               <source src={video2} type="video/mp4" />
             </video>
           </div>
